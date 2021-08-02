84a24a72e73372a12325aec08c73f7678a9acea1
緑川実生 <mio@midorikawa-MacBook-Air.local>
2021-08-03 00:54:30 +0900

    valgrind-debugOK,maybe

--- get_next_line.c
+++ get_next_line.c
@@ -1,9 +1,57 @@
 #include "get_next_line.h"
 
-static int	create_line(char **line, char *buf)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*p;
 	size_t	i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	i = ft_strlen(s1) + ft_strlen(s2);
+	p = (char *)malloc(sizeof(char) * (i + 1));
+	if (p == NULL)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		p[i++] = *s1++;
+	while (*s2)
+		p[i++] = *s2++;
+	p[i] = '\0';
+	return (p);
+}
+
+void	*ft_calloc(size_t n, size_t size)
+{
+	void	*p;
+
+	p = (void *)malloc(size * n);
+	if (p == NULL)
+		return (NULL);
+	ft_memset(p, 0, size * n);
+	return (p);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t			j;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (dst == NULL || src == NULL)
+		return (0);
+	j = 0;
+	while (j++ < size && *s)
+		*d++ = *s++;
+	if (size != 0)
+		*d = '\0';
+	return (ft_strlen(s));
+}
+
+int	create_line(char **line, char *buf)
+{
+	char	*p;
 	char	*tmp;
 	char	*tmp2;
 	int		f;
@@ -16,17 +64,15 @@ static int	create_line(char **line, char *buf)
 		free (tmp);
 		tmp = ft_substr(p + 1, 0, ft_strlen(p + 1));
 		ft_strlcpy(buf, tmp, ft_strlen(tmp));
-		free(tmp);
 		f = 0;
 	}
 	else
 	{
-		tmp = ft_substr(buf, 0, ft_strlen(buf));
-		tmp2 = ft_strjoin(*line, tmp);
-		ft_bzero(buf, 3 + 1);
-		free(tmp);
+		tmp2 = ft_strjoin(*line, buf);
+		ft_memset(buf, 0, BUFFER_SIZE + 1);
 		f = 1;
 	}
+	free(tmp);
 	free(*line);
 	*line = tmp2;
 	return (f);
@@ -42,24 +88,21 @@ char	*get_next_line(int fd)
 	f = 1;
 	line = ft_strdup("");
 	if (buf == NULL)
-		buf = (char *)ft_calloc(4, sizeof(char));
+		buf = (char *)ft_calloc(BUFFER_SIZE, sizeof(char));
 	i = 0;
 	while (f)
 	{
-		if (buf[0] != '\0' && i != 0)
+		if (buf[0] != '\0')
 			f = create_line(&line, buf);
 		if (f == 1)
 		{
-			if (read(fd, buf, 3) <= 0)
+			if (read(fd, buf, BUFFER_SIZE) <= 0)
 			{
-				if (i == 0)
+				if (i++ == 0)
 					return (NULL);
-				else
-					break ;
+				break ;
 			}
 		}
-		i++;
 	}
-	free(buf);
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,8 @@ char	*ft_strdup(const char *src);
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 void	*ft_calloc(size_t n, size_t size);
-void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+void	*ft_memset(void *buf, int ch, size_t n);
+int		create_line(char **line, char *buf);
 
 #endif
