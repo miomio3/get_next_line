60e807396a8ea3100de45c75070cdf00742f5a21
緑川実生 <mio@midorikawa-MacBook-Air.local>
2021-08-03 17:20:32 +0900

    namechange,Tester-can-move

--- get_next_line.h
+++ get_next_line.h
@@ -9,12 +9,12 @@
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *src);
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 void	*ft_calloc(size_t n, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 void	*ft_memset(void *buf, int ch, size_t n);
 int		create_line(char **line, char *buf);
+char	*ft_strdup(const char *src);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -41,13 +41,21 @@ char	*ft_strchr(const char *s, int c)
 char	*ft_strdup(const char *src)
 {
 	char	*dest;
+	size_t	i;
 
-	dest = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
+	i = 0;
+	while (src[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
 	if (dest == NULL)
 		return (NULL);
-	while (*src)
-		*dest++ = *src++;
-	*dest = '\0';
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
 
